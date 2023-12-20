a3da807e27e982943ff7116bcac1af889b438daa
Vishal Sharma <87796097+Vishal9971@users.noreply.github.com>
2023-12-20 15:18:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
-// seedDB();
+seedDB();
 app.use(clientRoutes);
 
 const PORT = 8080;
